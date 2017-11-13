31dee127e14d839137a44f4369e6702a04901464
김휘겸 <breakstorm00@gmail.com>
2017-11-13 17:47:01 +0900

    Update mongoose connection

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ var mongoose = require('mongoose');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;
+
 // var router = require('./routes')
 // app.use('/', router)
 var router = require('./routes')(app)
 
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function() {
+	console.log('Connect to mongodb server');
+})
+mongoose.connect('mongodb://localhost/mongodb_tutorial');
 
+var port = process.env.PORT || 8080;
 var server = app.listen(port, function() {
 	console.log("Express server has started on port " + port);
 })
